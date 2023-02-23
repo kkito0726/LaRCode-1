289ef5dca829fd2e06ff4239961eb77c423613d0
kkito0726 <k.kito0726@gmail.com>
2023-02-23 18:42:36 +0900

    回答を完了

--- A1_ManyA+BProblems.py
+++ A1_ManyA+BProblems.py
@@ -6,6 +6,9 @@ A問題
 '''
 
 # TODO: 入力を受け取る
-
+N = int(input())
+nums = [list(map(int, input().split())) for _ in range(N)]
 
 # TODO: 期待する出力になるようにコードを書く
+for num in nums:
+  print(num[0] + num[1])
--- B_QualB.py
+++ B_QualB.py
@@ -6,6 +6,18 @@ A問題
 '''
 
 # TODO: 入力を受け取る
-
+N, K = map(int, input().split())
+S = input()
 
 # TODO: 期待する出力になるようにコードを書く
+ans = ""
+for i in range(N):
+  if K==0 or S[i]=="x":
+    ans += "x"
+    continue
+  
+  if S[i] == "o":
+    ans += "o"
+    K -= 1
+    
+print(ans)
