60edea3f183c279e5d95547994e3ceedc196dfab
kkito0726 <k.kito0726@gmail.com>
2023-02-23 18:16:52 +0900

    initial commit

---
+++ A1_ManyA+BProblems.py
@@ -0,0 +1,11 @@
+'''
+URL: https://atcoder.jp/contests/abc288/tasks/abc288_a
+
+Toyota Programming Contest 2023 Spring Qual A（AtCoder Beginner Contest 288）
+A問題
+'''
+
+# TODO: 入力を受け取る
+
+
+# TODO: 期待する出力になるようにコードを書く
---
+++ A2_ContestResult.py
@@ -0,0 +1,11 @@
+'''
+URL: https://atcoder.jp/contests/abc290/tasks/abc290_a
+Toyota Programming Contest 2023 Spring Qual B（AtCoder Beginner Contest 290）
+
+A問題
+'''
+
+# TODO: 入力を受け取る
+
+
+# TODO: 期待する出力になるようにコードを書く
