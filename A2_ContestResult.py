41aef905f03733f0f652a08a38b863f9a4e6410a
shunsukeshi <shunsukeshiomi0914@gmail.com>
2023-03-02 16:58:03 +0900

    A2回答完了

--- A2_ContestResult.py
+++ A2_ContestResult.py
@@ -7,5 +7,23 @@ A問題
 
 # TODO: 入力を受け取る
 
+n, m = map(int, input().split())  
+list = [list(map(int, input().split())) for _ in range(2)]
+
+num_list = list[0]
+index_list = list[1]
 
 # TODO: 期待する出力になるようにコードを書く
+
+answer = []
+
+for i in index_list:
+    for i2, n in enumerate(num_list):
+        if i-1 == i2:
+            answer.append(n)
+
+print(sum(answer))
+
+
+
+
