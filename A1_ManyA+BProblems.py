e343af81496581c8ad2f320548d60cb389ccb841
shunsukeshi <shunsukeshiomi0914@gmail.com>
2023-03-02 14:03:03 +0900

    A1回答完了

--- A1_ManyA+BProblems.py
+++ A1_ManyA+BProblems.py
@@ -6,6 +6,11 @@ A問題
 '''
 
 # TODO: 入力を受け取る
-
+n = int(input())  # nは入力回数
+num_list = [list(map(int, input().split())) for _ in range(n)]
 
 # TODO: 期待する出力になるようにコードを書く
+import numpy as np
+for i in range(n):
+    answer = np.sum(num_list[i])
+    print(answer)
