1aad4135ae731526de49540046174261be2a4078
kkito0726 <k.kito0726@gmail.com>
2023-03-03 17:16:51 +0900

    ♻️ 変数名の変更

--- A2_ContestResult.py
+++ A2_ContestResult.py
@@ -8,11 +8,11 @@ A問題
 # TODO: 入力を受け取る
 N, M = map(int, input().split())
 tests = list(map(int, input().split()))
-choice_idx = list(map(int, input().split()))
+id_list = list(map(int, input().split()))
 
 # TODO: 期待する出力になるようにコードを書く
 point = 0
 for i in range(M):
-  point += tests[choice_idx[i] - 1]
+  point += tests[id_list[i] - 1]
   
 print(point)
