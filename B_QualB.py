770cc75b9d69d756e18d5533ab3a1f597d0d87c1
S.Shiomi <shunsukeshiomi0914@gmail.com>
2023-03-05 18:06:09 +0900

    B1回答

--- B_QualB.py
+++ B_QualB.py
@@ -8,4 +8,16 @@ A問題
 # TODO: 入力を受け取る
 
 
+
+
+
 # TODO: 期待する出力になるようにコードを書く
+N, K = map(int, input().split()) #Nは参加者、Kは予選通過者
+S = [s for s in input()] #Sは文字列
+answer = []
+for p in S:
+    if p == "o" and answer.count("o") < K:
+        answer.append("o")
+    else:
+        answer.append("x")
+print(answer)
