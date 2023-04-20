10479fbe77499ffe43c5c244e6877910aaa3fe2e
redfieldf <redfieldf@example.com>
2023-04-20 14:47:08 +0900

    初めてのコミットです

--- A1_ManyA+BProblems.py
+++ A1_ManyA+BProblems.py
@@ -6,6 +6,9 @@ A問題
 '''
 
 # TODO: 入力を受け取る
-
+N = int(input())
 
 # TODO: 期待する出力になるようにコードを書く
+for i in range(N):
+	A, B = map(int, input().split())
+	print(A+B)
--- A2_ContestResult.py
+++ A2_ContestResult.py
@@ -6,6 +6,9 @@ A問題
 '''
 
 # TODO: 入力を受け取る
-
+N, M = map(int, input().split())
 
 # TODO: 期待する出力になるようにコードを書く
+for i in range(M):
+    A, B = map(int, input().split())
+    print (A, B)
