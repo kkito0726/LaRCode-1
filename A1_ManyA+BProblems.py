cb65e855cf464cefd3b4480b21fa5fccd9f07b76
Takumi <takumi.yamaguchi.2d@stu.hosei.ac.jp>
2023-08-22 18:10:43 +0900

    A1解答完了

--- A1_ManyA+BProblems.py
+++ A1_ManyA+BProblems.py
@@ -6,6 +6,8 @@ A問題
 '''
 
 # TODO: 入力を受け取る
-
-
+N = int(input())
+numbers = [list(map(int, input().split())) for i in range(N)]
 # TODO: 期待する出力になるようにコードを書く
+for i in range(N):
+    print(numbers[i][0] + numbers[i][1])
