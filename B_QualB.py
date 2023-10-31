20120545ca26a49e650988b111a3a6db939cbdf7
Takumi <takumi.yamaguchi.2d@stu.hosei.ac.jp>
2023-10-31 20:58:02 +0900

    B問題回答しました

--- B_QualB.py
+++ B_QualB.py
@@ -6,6 +6,18 @@ A問題
 '''
 
 # TODO: 入力を受け取る
-
+N, K = map(int, input().split())
+request = input()
 
 # TODO: 期待する出力になるようにコードを書く
+advance = []
+for i in range(len(request)):
+    if K > 0:
+       if request[i] == 'o':
+           K -= 1
+           advance.append('o')
+       elif request[i] == 'x':
+           advance.append('x')
+    else:
+        advance.append('x')
+print(''.join(advance))
