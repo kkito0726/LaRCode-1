6636d3c6f242f7173751aba6cf66442fc0537b7a
Takumi <takumi.yamaguchi.2d@stu.hosei.ac.jp>
2023-10-31 21:03:31 +0900

    A2回答完了

--- A2_ContestResult.py
+++ A2_ContestResult.py
@@ -6,6 +6,12 @@ A問題
 '''
 
 # TODO: 入力を受け取る
-
+N, M = map(int, input().split())
+score = list(map(int, input().split()))
+answer = list(map(int, input().split()))
 
 # TODO: 期待する出力になるようにコードを書く
+ans = 0
+for a in answer:
+    ans += score[a-1]
+print(ans)
