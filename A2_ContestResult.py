43890b3361ba6636c17eae0a26a5aca9aa93f629
AyuSaka <ayuri.sakaguchi.2g@stu.hosei.ac.jp>
2023-11-07 18:30:59 +0900

    A2解答終了

--- A2_ContestResult.py
+++ A2_ContestResult.py
@@ -7,5 +7,13 @@ A問題
 
 # TODO: 入力を受け取る
 
+n, m = map(int, input().split())
+points = list(map(int, input().split()))
+index = list(map(int, input().split()))
 
 # TODO: 期待する出力になるようにコードを書く
+score = 0
+for i in index:
+    score += points[i-1]
+
+print(score)
