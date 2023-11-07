c3e41c071416f77401c9092d70a5451aa49e3729
takaaki <takaaki0210123@gmail.com>
2023-11-07 18:40:45 +0900

    A2回答完了しました

--- A2_ContestResult.py
+++ A2_ContestResult.py
@@ -6,6 +6,15 @@ A問題
 '''
 
 # TODO: 入力を受け取る
-
-
+N,M = map (int, input().split())
+yeah = [list(map(int, input().split())) for _ in range(N)]
+print(N,M)
+print(yeah)
 # TODO: 期待する出力になるようにコードを書く
+Score = 0
+for i in range(M):
+    y = yeah[1][i]-1
+    H = yeah[0][y]
+    Score += H
+print(Score)
+    
