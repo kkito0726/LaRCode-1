8205bf0a35b259318872afe96077c5dbdcafea2b
AyuSaka <ayuri.sakaguchi.2g@stu.hosei.ac.jp>
2023-11-07 18:04:00 +0900

    A1解答完了

--- A1_ManyA+BProblems.py
+++ A1_ManyA+BProblems.py
@@ -6,6 +6,11 @@ A問題
 '''
 
 # TODO: 入力を受け取る
-
+N = int(input())
+nums = [list(map(int, input().split())) for _ in range(N)]
+# print(N)
+# print(nums)
 
 # TODO: 期待する出力になるようにコードを書く
+for i in range(N):
+    print(nums[i][0] + nums[i][1])
