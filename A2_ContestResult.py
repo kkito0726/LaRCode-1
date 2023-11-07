1f444c55b12437504df06d437379cbb2a8187f3a
takaaki <takaaki0210123@gmail.com>
2023-11-07 20:03:42 +0900

    A2回答完了しました

--- A2_ContestResult.py
+++ A2_ContestResult.py
@@ -7,9 +7,7 @@ A問題
 
 # TODO: 入力を受け取る
 N,M = map (int, input().split())
-yeah = [list(map(int, input().split())) for _ in range(N)]
-print(N,M)
-print(yeah)
+yeah = [list(map(int, input().split())) for _ in range(2)]
 # TODO: 期待する出力になるようにコードを書く
 Score = 0
 for i in range(M):
