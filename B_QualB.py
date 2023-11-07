a0576a0c6232e9a79a2c06f570b38c4ed59d63d2
takaaki <takaaki0210123@gmail.com>
2023-11-07 20:04:43 +0900

    B回答完了しました

--- B_QualB.py
+++ B_QualB.py
@@ -6,6 +6,20 @@ A問題
 '''
 
 # TODO: 入力を受け取る
-
-
+N, K = map(int, input().split())
+S = input()
 # TODO: 期待する出力になるようにコードを書く
+join = ''
+l = 0
+for i in range(N):
+    if l < K:
+        if S[i] == 'o':
+            l += 1
+            join += 'o'
+        if S[i] == 'x':
+            join += 'x'
+    else:
+        join += 'x'
+print(join)
+
+    
