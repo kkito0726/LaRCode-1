8deccdd1ccf44243cd6588d5560868a7df15aaa8
unabook <unabook91@gmail.com>
2023-11-21 18:14:08 +0900

    A1問題解答完了

--- A1_ManyA+BProblems.py
+++ A1_ManyA+BProblems.py
@@ -7,5 +7,18 @@ A問題
 
 # TODO: 入力を受け取る
 
+N = int(input())
+num_list = [list(map(int, input().split())) for _ in range(N)]
+# print(N)
+# print(num_list)
+
+# for i in range(N):
+#      print(nums[i][0] + nums[i][1])
+# for num in nums:
+#   print(num[0] + num[1])
+
+for i in range(N):
+    print(num_list[i][0] + num_list[i][1])
+
 
 # TODO: 期待する出力になるようにコードを書く
