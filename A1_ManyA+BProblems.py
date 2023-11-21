e3e0259cfe721c8197ecf8fb3a4ba25527c8c183
sakamoto <shion.0224@icloud.com>
2023-11-21 18:27:21 +0900

    鬼頭さんいつもありがとう

--- A1_ManyA+BProblems.py
+++ A1_ManyA+BProblems.py
@@ -6,6 +6,10 @@ A問題
 '''
 
 # TODO: 入力を受け取る
-
+n = int(input())
+num_list = [list(map(int, input().split())) for _ in range(n)]
 
 # TODO: 期待する出力になるようにコードを書く
+
+for num in num_list :
+    print(num[0]+num[1])
