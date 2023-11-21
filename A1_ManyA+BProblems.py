0b51328c8641e06260a3bc041b4b31c7a37a5b3b
morishin <shinnosuke.mori.9q@stu.hosei.ac.jp>
2023-11-21 18:07:26 +0900

    A1解答完了

--- A1_ManyA+BProblems.py
+++ A1_ManyA+BProblems.py
@@ -8,4 +8,17 @@ A問題
 # TODO: 入力を受け取る
 
 
-# TODO: 期待する出力になるようにコードを書く
+# TODO: 期待する出力になるようにコードを
+
+
+
+# nums = map(int, input().split())
+# print(nums)
+# num_list = list(map(int, imput().split()))
+
+n = int(input())
+num_list = [list(map(int, input().split())) for _ in range(n)]
+# print(num_list)
+
+for i in range(n):
+    print(num_list[i][0] + num_list[i][1])
