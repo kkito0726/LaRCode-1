568157363a97a1dc97cada00a3ced01838e51ea8
morishin <shinnosuke.mori.9q@stu.hosei.ac.jp>
2023-11-21 18:44:57 +0900

    A2解答完了

--- A2_ContestResult.py
+++ A2_ContestResult.py
@@ -6,6 +6,16 @@ A問題
 '''
 
 # TODO: 入力を受け取る
+M,N = map(int, input().split())
+tensu = list(map(int, input().split()))
+monndaisu = list(map(int, input().split()))
+# print(M,N)
+# print(tensu)
+# print(monndaisu)
 
 
 # TODO: 期待する出力になるようにコードを書く
+score = 0 
+for i in monndaisu:
+    score += tensu[i-1]
+print(score)
