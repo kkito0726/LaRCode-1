f2b4053b0e39dd04e3e68a55322d78e42d4e658f
sakamoto <shion.0224@icloud.com>
2023-11-28 16:27:44 +0900

    鬼頭さんお世話様です

--- A2_ContestResult.py
+++ A2_ContestResult.py
@@ -6,6 +6,11 @@ A問題
 '''
 
 # TODO: 入力を受け取る
-
-
+N, M = map(int, input().split())
+score_list = list(map(int, input().split()))
+index = list(map(int, input().split()))
 # TODO: 期待する出力になるようにコードを書く
+ans = 0
+for id in index:
+    ans += score_list[id - 1]
+print(ans)
