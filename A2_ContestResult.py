0d9c1159cd9a774488e3a9a5d32c501e6dab446c
conne <kanki1359@icloud.com>
2023-12-18 17:14:56 +0900

    3回目

--- A2_ContestResult.py
+++ A2_ContestResult.py
@@ -6,14 +6,3 @@ A問題
 '''
 
 # TODO: 入力を受け取る
-a,b = map(int, input().split())
-#print(a,b)
-num_list = []
-for i in range(2):
-    num_list.append(list(map(int,input().split())))
-#print(num_list)
-# TODO: 期待する出力になるようにコードを書く
-a = 0
-for i in range(b):
-    a += num_list[0][num_list[1][i]-1]
-print(a)
