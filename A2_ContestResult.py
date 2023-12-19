edfab5dd14932416db393443607c815646f8ce3a
conne <kanki1359@icloud.com>
2023-12-19 15:12:09 +0900

    3回目

--- A2_ContestResult.py
+++ A2_ContestResult.py
@@ -6,3 +6,12 @@ A問題
 '''
 
 # TODO: 入力を受け取る
+a,b = map(int, input().split())  # 3個の数字の入力を受け取る                      # 出力を確認
+num_list = list(map(int, input().split()))  # n個の数字がリストに格納される
+num_list2 = list(map(int, input().split()))  # n個の数字がリストに格納される
+
+
+score = 0
+for i in range(b) :
+    score += num_list[num_list2[i]-1]
+print(score)
